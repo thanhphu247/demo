50b488b5a36dc4e7d7054f92c7d3d4732ea86cc3
thanhphu247 <phuhathanh247@gmail.com>
2020-10-20 10:33:08 +0700

    Add function login and update UI/UX

--- src/App.js
+++ src/App.js
@@ -1,87 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ListItems from './list-items'
+// import logo from './logo.svg';
+// import './App.css';
+// import Login from '../src/login/LoginForm';
+// import List from '../src/list/List';
+// import ListItems from './list-items'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+// import { Route, Router } from 'react-router';
+import Routes from './routes/Routes';
 
 library.add(faTrash)
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      items:[],
-      currentItem:{
-        text:'',
-        key:''
-      }
-    }
-    this.addItem = this.addItem.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.setUpdate = this.setUpdate.bind(this);
-  }
-  addItem(e){
-    e.preventDefault();
-    const newItem = this.state.currentItem;
-    if(newItem.text !==""){
-      const items = [...this.state.items, newItem];
-    this.setState({
-      items: items,
-      currentItem:{
-        text:'',
-        key:''
-      }
-    })
-    }
-  }
-  handleInput(e){
-    this.setState({
-      currentItem:{
-        text: e.target.value,
-        key: Date.now()
-      }
-    })
-  }
-  deleteItem(key){
-    const filteredItems = this.state.items.filter(item =>
-      item.key!==key);
-    this.setState({
-      items: filteredItems
-    })
-
-  }
-  setUpdate(text,key){
-    console.log("items:"+this.state.items);
-    const items = this.state.items;
-    items.map(item=>{      
-      if(item.key===key){
-        console.log(item.key +"    "+key)
-        item.text= text;
-      }
-    })
-    this.setState({
-      items: items
-    })
-    
-   
-  }
+  
  render(){
   return (
     <div className="App">
-      <header>
-        <form id="to-do-form" onSubmit={this.addItem}>
-          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
-          <button type="submit">Add</button>
-        </form>
-        <p>{this.state.items.text}</p>
-        <div className="listBx">
-          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
-        </div>
-          
-        
-      </header>
+      <Routes/>
     </div>
   );
  }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Login from './login/LoginForm';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
